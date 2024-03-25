7d1ac6a5227938252296f797450a8be1c9de33ef
NikyS02 <schonova.nikola@gmail.com>
2024-03-25 12:03:59 +0100

    ZU04-ii WIP
    prace na nactení ze souboru ->neuklada se 1? hodnota ze souboru

--- ZadaniUkolu04ii-rosireni_kvadrativcka_rovnice/src/Main.java
+++ ZadaniUkolu04ii-rosireni_kvadrativcka_rovnice/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        provider provider = new provider();
+        Double[][] in = provider.NacteniDat_DataKvadratickeRovnice();
+        for(Double[] d : in) {
+            for (Double x : d) {
+                System.out.print(x);
+            }
+            System.out.println();
+        }
     }
+
 }
